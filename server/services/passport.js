cf46d29ba221c0fe58d5e1e1f811fd4874c76db1
Dev Sanghvi <dys27@cornell.edu>
2019-05-13 15:20:19 -0400

    setting up axios and redux thunk

--- server/services/passport.js
+++ server/services/passport.js
@@ -22,18 +22,13 @@ passport.use(new GoogleStrategy({
     callbackURL: '/auth/google/callback',
     proxy: true
 },
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id })
-            .then((existingUser) => {
-                if (existingUser) {
-                    //we already have the userID
-                    done(null, existingUser);
-                } else {
-                    //make a new user
-                    new User({ googleId: profile.id })
-                        .save()
-                        .then(user => done(null, user));
-                }
-            })
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id })
+        if (existingUser) {
+            return done(null, existingUser);
+        }
+        const user = await new User({ googleId: profile.id }).save()
+        done(null, user);
+
     })
 );
